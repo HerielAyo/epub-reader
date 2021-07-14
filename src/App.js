69f6ae3afa1ab141d195c8f127da07fddaf371d0
HerielAyo <52042111+HerielAyo@users.noreply.github.com>
2021-07-14 15:41:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  ReactReader,
+  ReactReaderStyle,
+  EpubViewStyle,
+  EpubView,
+
+} from 'react-reader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ position: "relative", height: "100%" }}>
+      <p>Epub Reader</p>
+      {" "}
+      <ReactReader
+        url={"/bogoriana.epub"}
+        title={"Bogoriana"}
+        location={"epubcfi(/6/2[cover]!/6)"}
+        locationChanged={epubcifi => console.log(epubcifi)}
+      />
     </div>
   );
 }
