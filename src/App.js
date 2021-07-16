abe560deecdd629646679c0530ecf3d746fa6d8d
HerielAyo <52042111+HerielAyo@users.noreply.github.com>
2021-07-16 20:13:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string'
+import { useState  } from 'react';
 import {
   ReactReader,
-  ReactReaderStyle,
-  EpubViewStyle,
-  EpubView,
+  EpubView 
 
 } from 'react-reader';
 
+
+
 function App() {
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('pdfName');
+ //const loc = queryString.parse(urlParams)
+ console.log(myParam)
+
   return (
+    <div style = {{ height:500, width: "100%"}}>
     <div style={{ position: "relative", height: "100%" }}>
       <p>Epub Reader</p>
       {" "}
-      <ReactReader
-        url={"/bogoriana.epub"}
-        title={"Bogoriana"}
+    
+      <ReactReader 
+        // url={"./files/bogoriana.epub"}
+        url={"/upload/epub/"+ myParam
+      }
+        // title={"Alice"}
         location={"epubcfi(/6/2[cover]!/6)"}
         locationChanged={epubcifi => console.log(epubcifi)}
+        tocChanged={toc => console.log(toc)}
+        swipeable = {true}
+        showToc = {true}
       />
+    </div>
     </div>
   );
 }
